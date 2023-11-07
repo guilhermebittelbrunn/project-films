6682e9b31f9db58f17db857fb313e9ad9f3e6f40
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-11-07 17:44:25 -0300

    Update router.js

--- back/api/router.js
+++ back/api/router.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const Router = express.Router();
-const { UserController, MovieController, GenreController, authenticate } = require('./controllers/index');
+const {
+    UserController,
+    MovieController,
+    GenreController,
+    ListController,
+    authenticate,
+} = require('./controllers/index');
 
 Router.post('/user/create', UserController.post);
 Router.post('/user/login', UserController.postLogin);
@@ -14,5 +20,9 @@ Router.get('/movie/:id', authenticate, MovieController.get);
 
 Router.get('/genres', authenticate, GenreController.getAll);
 Router.get('/genre/:id', authenticate, GenreController.get);
+
+Router.get('/lists/:id', authenticate, ListController.getAll);
+// Router.post('/lists', authenticate, ListController.post)
+Router.post('/lists', authenticate, ListController.post);
 
 module.exports = Router;
